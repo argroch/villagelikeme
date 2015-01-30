b0ebddf5b16175c75603df0dd13df8d383657408
Aaron Groch <ar.groch@gmail.com>
2015-01-30 18:03:56 -0500

    randomized bg images yay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def randomized_background_image
+	  images = ["assets/brooklyn-cafe.jpg", "assets/berkeley-elmwood.jpg", "assets/atlantic-station.jpg","assets/little-five-points.jpg","assets/neighborhood-pic1.jpg"]
+	  images[rand(images.size)]
+	end
+
 end
