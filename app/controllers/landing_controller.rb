86d355e22edfe1aae910cc845c1f5c667ec16008
Aaron Groch <ar.groch@gmail.com>
2015-01-30 17:47:56 -0500

    the way I have things set up I couldnt figure out how to show validation errors so I just tossed those for now

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,17 +1,13 @@
 class LandingController < ApplicationController
   def index
-
   end
 
   def thanks
-
   	@mover = Mover.new
   	@mover.current_city = params[:current_city]
   	@mover.moving_to = params[:moving_to]
   	@mover.email = params[:email]
-
-  	@mover.save
-
+		@mover.save
   end
 
 end
--- app/models/mover.rb
+++ app/models/mover.rb
@@ -1,9 +1,3 @@
 class Mover < ActiveRecord::Base
-
-	validates :current_city, :moving_to, :email, presence: true
-
-	validates :email, uniqueness: true
-
-	validates :email, :email_format => { :message => 'is not looking good' }
 	
 end
