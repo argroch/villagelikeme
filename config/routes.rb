1851770d48ba6cd57912031ff6cacfbf7e3740b5
Aaron Groch <ar.groch@gmail.com>
2015-02-09 20:57:26 -0500

    added a page, gonna work with optimizely, changed bg images

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   post '/thanks' => 'landing#thanks'
 
+  post '/your_scene' => 'landing#your_scene'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
